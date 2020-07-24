8897da2d5a26e5242d62775adc13c2f30c876d83
andresgr20 <andresgr2094@gmail.com>
2020-07-24 00:22:42 -0400

    Added more stylized buttons, drop down for adding drivers, and made the navbar

--- rose-dispatch/src/App.js
+++ rose-dispatch/src/App.js
@@ -1,34 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <ul className="topLinks">
-        <li>
-          <a href=""> Update Drivers</a>  // Used to update the list of drivers. It will popup to change the driver info (email, name, contact ), create a new one or delete a driver
-        </li>
-        <li>
-          <a href=""> Add task</a>  // Create a new task for  a user, if there is a conflict with a task it will recommned another driver or another time for the same driver 
-          // A task should have picture,Driver, Type, time duration, Description, Location,nationality
+        <nav className="navBar">
+          <img src={logo} className="logo" alt="logo" />
+          <ul className="barButtons">
+            <li>
+                      {/* // Used to update the list of drivers. It will popup to change the driver info (email, name, contact ), create a new one or delete a driver */}
+                  <Dropdown as={ButtonGroup}>
+                <Button variant="success">Add Driver</Button>
+
+                <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">John Mask</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Andres Me</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Grace Science</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </li>
+            <li>
+          <Button> Add task</Button>  
+          {/* // Create a new task for  a user, if there is a conflict with a task it will recommned another driver or another time for the same driver 
+          // A task should have Driver, Type, time duration, Description, Location,nationality
           // after adding sends and invite to the person in the email
-          // pick up, drop off, other are drop down (if selects pick up or drop off input address) other is blank
+          // pick up, drop off, other are drop down (if selects pick up or drop off input address) other is blank */}
         </li>
-      </ul>
+          </ul>
+          </nav>
 
-      // Maybe add a text notif when a task is added or email  
+      {/* // Maybe add a text notif when a task is added or email  
 
 
-      //Have drop down with drivers here can use react native. This will change the calendar
-      <button>Email</button> // Will email the calendar as a pdf to the driver based on the contact info for a week
-      <button>Print</button> // turn into pdf and print calendar for the week
-      // Dropdown setting the download range
-      <button>Download</button> // pop up to select range turn the current calendar into csv 
+      //Have drop down with drivers here can use react native. This will change the calendar */}
+      <button>Email</button> 
+      {/* // Will email the calendar as a pdf to the driver based on the contact info for a week */}
+      {/* // turn into pdf and print calendar for the week */}
+      <button>Print</button> 
+      {/* // Dropdown setting the download range */}
+      <button>Download</button> 
+      {/* // pop up to select range turn the current calendar into csv  */}
 
-      // Calendar will be here 
+      {/* // Calendar will be here  */}
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -41,7 +60,6 @@ function App() {
         >
           Learn React
         </a> */}
-      </header>
     </div>
   );
 }
