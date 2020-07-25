c81af85eeffdc80b35705f9ac513b1dc7f3e3564
andresgr20 <andresgr2094@gmail.com>
2020-07-25 16:44:54 -0400

    Default calendar from tui toast

--- rose-dispatch/src/App.js
+++ rose-dispatch/src/App.js
@@ -4,8 +4,32 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Dropdown from 'react-bootstrap/Dropdown'
 import Button from 'react-bootstrap/Button'
 import './App.css';
+import Calendar from 'tui-calendar'; /* ES6 */
+import "tui-calendar/dist/tui-calendar.css";
+
+// If you use the default popups, use this.
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
 
 function App() {
+
+  var Calendar = require('tui-calendar'); /* CommonJS */
+require("tui-calendar/dist/tui-calendar.css");
+
+// If you use the default popups, use this.
+require("tui-date-picker/dist/tui-date-picker.css");
+require("tui-time-picker/dist/tui-time-picker.css");
+
+var calendar = new Calendar('#calendar', {
+  defaultView: 'month',
+  taskView: true,
+  template: {
+    monthDayname: function(dayname) {
+      return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+    }
+  }
+});
+
   return (
     <div className="App">
         <nav className="navBar">
